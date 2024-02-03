39d749e9e6bebdff508ecbed2e1cbdb04ca00658
Kimberly Cooper <133775257+KCooper1417@users.noreply.github.com>
2024-02-03 11:44:52 -0500

    controller

--- src/main/java/com/moneypicks/demo/MoneyPicksController.java
+++ src/main/java/com/moneypicks/demo/MoneyPicksController.java
@@ -1,9 +1,13 @@
 package com.moneypicks.demo;
 
+import java.util.Scanner;
+
+import org.hibernate.mapping.List;
+
 public class MoneyPicksController {
     public static void main(String[] args){
 
-        List<INTEGER> winningNumbers = new ArrayList<>();
+        List<Integer> winningNumbers = new ArrayList<>();
         Random random = new Random();
 
         for(int i = 0; i < 3; i++){
@@ -11,8 +15,20 @@ public class MoneyPicksController {
                 int winningNumber = random.nextInt(999) + 1;
                 if (!winningNumbers.contains(winningNumber)) {
                     winningNumbers.add(winningNumber);
+                    break;
                 }
             }
+        }
+
+        System.out.println(winningNumbers);
+        System.out.println("Enter your 3 picks here (inclusive)");
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer>
+        for (int i = 0; i < 3; i++){
+            String numberString = scanner.nextLine();
+            int number = Integer.parseInt(numberString);
+            guessedNumbers.add(number);
         }
     }
 
