a3864239d47a314a3b957ace7fbe15e42e151136
Kimberly Cooper <133775257+KCooper1417@users.noreply.github.com>
2024-02-10 12:39:06 -0500

    fixed index and result.html

--- src/main/java/com/moneypicks/demo/MoneyPicksController.java
+++ src/main/java/com/moneypicks/demo/MoneyPicksController.java
@@ -1,20 +1,31 @@
 package com.moneypicks.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 public class MoneyPicksController {
 
-    private final MoneyPicksService moneyPicksService;
+    // private final MoneyPicksService moneyPicksService;
 
     public MoneyPicksController(MoneyPicksService moneyPicksService) {
         this.moneyPicksService = moneyPicksService;
+    }
+
+    @Autowired
+    private MoneyPicksService moneyPicksService;
+
+    @GetMapping("/generateWinningNumbers")
+    @ResponseBody
+    public List<Integer>generateWinningNumbers(){
+        return moneyPicksService.generateWinningNumbers();
     }
 
     @GetMapping("/pick")
